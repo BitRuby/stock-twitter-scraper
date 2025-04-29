959faf0117c5af89c0b1d3e47ee71d348b387446
Michał Zwierzyński <michal.zwierzynski16@gmail.com>
2025-04-29 09:43:16 +0200

    fix

--- Scraper.py
+++ Scraper.py
@@ -12,7 +12,7 @@ import os
 
 uri = os.environ.get('MONGODB_URI', None)
 client = MongoClient(uri, server_api=ServerApi('1'))
-PREDEFINED_DATE="2022-02-06"
+PREDEFINED_DATE="2021-04-29"
 
 try:
     client.admin.command('ping')
